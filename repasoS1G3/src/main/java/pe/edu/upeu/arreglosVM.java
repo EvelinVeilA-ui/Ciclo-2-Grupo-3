6921e4d95eaaca8e0d3b9a869753efc7f7ac717c
EvelinVeilA-ui <yeolveil@gmail.com>
2026-08-13 14:51:17 -0500

    Repaso vector

--- repasoS1G3/src/main/java/pe/edu/upeu/arreglosVM.java
+++ repasoS1G3/src/main/java/pe/edu/upeu/arreglosVM.java
@@ -1,7 +1,16 @@
 package pe.edu.upeu;
 
 public class arreglosVM {
+
+    public static void meses(){
+        String[] mes={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Setiembre","Octubre","noviembre","Diciembre"};
+        System.out.println("Tamaño: " +mes.length);
+        for (String m:mes){
+            System.out.println(m);
+        }
+        mes[11]="";
+    }
     public static void main(String[] args){
-        System.out.println("Hola Mundo");
+       meses();
     }
 }
