623ed414428d887799d8adc7aff75393dd99a72b
Evelin <yeolveil@gmail.com>
2026-08-19 07:44:33 -0500

    MATRIZ13

--- repasoS1G3/src/main/java/pe/edu/upeu/arreglosVM.java
+++ repasoS1G3/src/main/java/pe/edu/upeu/arreglosVM.java
@@ -56,6 +56,24 @@ public class arreglosVM {
         imprimirmatris(matrizF5(5,1));
         System.out.println("");
         imprimirmatris(matrizF11(5,1));
+    }
+      public static int[][] matrizf13(int tamanio, int num){
+        int [][] matriz = new int[tamanio][tamanio];
 
+        for (int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length - i; j++){
+                matriz[j][i]=num;
+                num++;
+            }
+        }
+        return matriz;
+    }
+      public static void main(String[] args) {
+       // meses();
+       // int [][] m={{1,2,6}, {3,4,2}, {5,6,7}};
+       // imprimirMatriz(m);
+        imprimirMatriz(matrizF5(6,1));
+        System.out.println("");
+        imprimirMatriz(matrizf13(5,1));
     }
 }
